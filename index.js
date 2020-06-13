b067ebf80d93765e40e1a5b63b51b29982d9f909
Paul Thrasher <thrashr888@gmail.com>
2020-06-13 10:54:35 -0700

    meh. html not possible because of CORS
    Signed-off-by: Paul Thrasher <thrashr888@gmail.com>

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
+import "regenerator-runtime";
+
 import React from "react";
 import ReactDOM from "react-dom";
 
-import simpleGit from "simple-git";
-const git = simpleGit(workingDirPath);
+// import simpleGit from "isomorphic-git";
+import git from "isomorphic-git";
+import http from "isomorphic-git/http/node";
 
-class HelloMessage extends React.Component {
+const WORKING_DIR_PATH = "./";
+const READ_FILE = "TEST.md";
+// const git = simpleGit(WORKING_DIR_PATH);
+
+console.log(`Using isomorphic-git version: ${git.version()}`);
+git
+  .getRemoteInfo({
+    http,
+    url: "https://github.com/isomorphic-git/isomorphic-git.git",
+  })
+  .then((data) => {
+    if (data && data.refs && data.refs.heads) {
+      console.log("List of remote branches");
+      console.log(Object.keys(data.refs.heads));
+    }
+  });
+
+async function main() {
+  // let l = await git.log({ file: READ_FILE });
+  // console.log(l);
+}
+main();
+
+class App extends React.Component {
   render() {
     return <div>Hello {this.props.name}</div>;
   }
 }
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+ReactDOM.render(<App />, mountNode);

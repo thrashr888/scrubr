6ee38432f62bc7c6d6208ea369380161a1309f1c
Paul Thrasher <thrashr888@gmail.com>
2020-06-13 10:30:12 -0700

    deps
    Signed-off-by: Paul Thrasher <thrashr888@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import simpleGit from "simple-git";
+const git = simpleGit(workingDirPath);
+
 class HelloMessage extends React.Component {
   render() {
     return <div>Hello {this.props.name}</div>;

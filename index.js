b8aefd7465658fdeb911fdfdedd67a141bf8348f
Paul Thrasher <thrashr888@gmail.com>
2020-06-14 12:37:20 -0700

    switch to blessed
    terminal-kit’s docs were hard to follow and i wasn’t able to switch out each text frame. it kept just overlapping all the text output.
    
    Signed-off-by: Paul Thrasher <thrashr888@gmail.com>

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const optionDefinitions = [
 ];
 const options = commandLineArgs(optionDefinitions);
 
-// terminal
-const term = require("terminal-kit").terminal;
-
 // git
 const simpleGit = require("simple-git");
 const WORKING_DIR_PATH = "./";
@@ -20,55 +17,97 @@ marked.setOptions({
   renderer: new TerminalRenderer(),
 });
 
-function printLog(log) {
-  return log.all.map((l, i) => {
-    return `${i}: ${l.message} (${l.hash.substr(0, 6)})`;
-  });
-}
+// terminal
+var blessed = require("blessed");
+var screen = blessed.screen({
+  smartCSR: true,
+});
+screen.title = "scrubr";
+let textBox = blessed.box({
+  top: "center",
+  left: "center",
+  width: "100%",
+  height: "100%",
+  content: "SCRUBR",
+  tags: true,
+  border: { type: "line" },
+  style: {
+    fg: "white",
+    bg: "black",
+    border: {
+      fg: "#f0f0f0",
+    },
+    scrollbar: {
+      bg: "blue",
+    },
+  },
+});
+screen.append(textBox);
+textBox.focus();
+screen.render();
+screen.key(["escape", "q", "C-c"], function (ch, key) {
+  return process.exit(0);
+});
 
 function tagName(commit) {
   if (!commit.refs.includes("tag: ")) return;
-
   return commit.refs.substr(5);
 }
 
+let SHOW_CACHE = {};
 async function printCommit(commit, filename, index) {
   let tag = tagName(commit);
-  console.log(`--- ${filename} ${tag || ""} ${index} ${commit.message}`);
+  let header = `{bold}${filename}{/bold} #${index + 1} ${
+    tag ? tag + " " : ""
+  }({green-fg}${commit.hash.substr(0, 6)}{/green-fg})\n${commit.message}\n\n`;
 
   try {
-    let show = await git.show(`${commit.hash}:${filename}`);
-    console.log(marked(show));
+    let key = `${commit.hash}:${filename}`;
+    let show = SHOW_CACHE[key] ? SHOW_CACHE[key] : await git.show(key);
+
+    textBox.setContent(header + marked(show), true);
   } catch (e) {
-    console.error(e.message);
+    textBox.setContent(header + e.message, true);
+    // console.error(e.message);
   }
+  screen.render();
+}
+
+async function printIndex(log, filename) {
+  let logs = log.map((l, i) => {
+    return `#${i + 1}: {bold}${l.message}{/bold} ({green-fg}${l.hash.substr(
+      0,
+      6
+    )}{/green-fg})`;
+  });
+  textBox.setContent(`{bold}${filename}{/bold}\n\n${logs.join("\n")}`);
+  screen.render();
 }
 
 async function main(filename) {
   let log = await git.log({ file: filename });
   let commits = log.all;
-  console.log(printLog(log));
+  printIndex(commits, filename);
 
-  let index = 0;
+  let index = -1;
   let commit = commits[index];
 
-  printCommit(commit, filename, index);
-
-  term.grabInput();
-
-  term.on("key", async function (name, matches, data) {
-    if (name === "CTRL_C") {
-      process.exit();
-    }
-
-    if (name === "RIGHT" || name === "DOWN") {
+  screen.key(["right", "left"], async function (ch, key) {
+    if (key.name === "right") {
       index++;
-      if (index >= commits.length - 1) index = commits.length - 1;
+      if (index > commits.length - 1) {
+        index = commits.length - 1;
+        return;
+      }
     }
-
-    if (name === "LEFT" || name === "UP") {
+    if (key.name === "left") {
       index--;
-      if (index <= 0) index = 0;
+      if (index < 0) {
+        index = 0;
+
+        printIndex(commits, filename);
+        return;
+      }
     }
 
     commit = commits[index];
